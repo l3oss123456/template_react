e31e604a0699ffc0d39035e77ce1503bc5048266
BossKung <bosskung@BossKungs-MacBook-Pro.local>
2022-07-19 17:37:40 +0700

    add loading if not found image

--- src/components/Images/index.js
+++ src/components/Images/index.js
@@ -1,36 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import * as R from "ramda";
-import { Image } from "antd";
+import { Image, Spin } from "antd";
 import image_config from "../../Configs/images";
 
 const Images = (props) => {
+  const [isLoading, setIsLoading] = useState(true);
+  let imageFound = false;
+
   const getSourceLocal = () => {
     let data = null;
     let base = image_config;
-    let imageFound = false;
 
     if (!R.isNil(props.name) && !R.isEmpty(props.name)) {
       imageFound = true;
       let splitNames = props.name.split(".");
 
       for (let x in splitNames) {
-        if (!base[splitNames[x].trim()]) {
+        // if (!base[splitNames[x].trim()]) {
+        //   base = null
+        //   imageFound = false
+        //   break
+        // }
+        if (!R.isNil(base[splitNames[x].trim()])) {
+          base = base[splitNames[x].trim()];
+        } else {
           base = null;
           imageFound = false;
           break;
         }
-
-        base = base[splitNames[x].trim()];
       }
 
       if (imageFound) {
         data = base.image;
       }
     }
-    let source = imageFound ? data : null;
+
+    let source = imageFound ? data : image_config.icon.notFoundImage.image;
 
     let imgWidth = 100,
       imgHeight = 100;
+
     if (imageFound) {
       //set width to image
       if (props.width) imgWidth = props.width;
@@ -39,25 +48,30 @@ const Images = (props) => {
       //set height to image
       if (props.height) imgHeight = props.height;
       else imgHeight = base.height;
+    } else {
+      imgWidth = `100%`;
+      imgHeight = `100%`;
     }
+
     return {
       width: imgWidth,
       height: imgHeight,
       src: source,
-      alt: props.alt ? props.alt : `...`,
       preview: props.preview ? props.preview : false,
+      draggable: false,
     };
   };
 
   const getSourceUri = () => {
-    let source = props.uri ? props.uri : null;
+    let source = props.uri ? props.uri : image_config.icon.notFoundImage.image;
 
     return {
       width: props.width ? props.width : 100,
       height: props.height ? props.height : 100,
       src: source,
-      alt: props.alt ? props.alt : `...`,
+      alt: source,
       preview: props.preview ? props.preview : false,
+      draggable: false,
     };
   };
 
@@ -69,17 +83,39 @@ const Images = (props) => {
 
     default:
       setting = getSourceLocal();
+      break;
   }
+
+  useEffect(() => {
+    const setLoading = () => {
+      if (imageFound) {
+        setIsLoading(false);
+      } else {
+        setTimeout(() => {
+          setIsLoading(false);
+        }, 3000);
+      }
+    };
+    setLoading();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [setting]);
+
   return (
-    <Image
-      {...setting}
-      style={{
-        ...props.style,
-        backgroundRepeat: "no-repeat",
-        backgroundAttachment: "fixed",
-        pointerEvents: "none",
-      }}
-    />
+    <>
+      {!isLoading ? (
+        <Image
+          {...setting}
+          style={{
+            ...props.style,
+            backgroundRepeat: "no-repeat",
+            backgroundAttachment: "fixed",
+            pointerEvents: "none",
+          }}
+        />
+      ) : (
+        <Spin spinning={isLoading} />
+      )}
+    </>
   );
 };
 
--- src/configs/images.js
+++ src/configs/images.js
@@ -5,5 +5,10 @@ export default {
       width: 60,
       height: 60,
     },
+    notFoundImage: {
+      image: require("../Assets/Images/not_found_image.png"),
+      width: 60,
+      height: 60,
+    },
   },
 };
